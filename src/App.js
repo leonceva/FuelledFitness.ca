07c749e9edbd7b987ce9ca4064bd48b9b62f94e8
leonceva <alberto.leon.c@gmail.com>
2022-06-29 19:22:29 -0600

    moved up

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navigation Bar */}
+      <header class='navbar navbar-expand-lg bg-dark'>
+        <Navbar>
+          
+        </Navbar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
