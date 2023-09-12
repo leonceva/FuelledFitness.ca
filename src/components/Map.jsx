8bfe981f3612421ec7a6afae5aa57e9e7cdd252e
leonceva <alberto.leon.c@gmail.com>
2023-09-11 20:49:56 -0400

    fixed ip address of backend server

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,7 +11,7 @@ export const Map = () => {
     useEffect(() => {
         const fetchKey = async () => {
             await axios
-                .post("http://localhost:8000/googleMapsApiKey")
+                .post("http://3.12.168.70:8000/googleMapsApiKey")
                 .then((res) => {
                     setApiKey(res.data);
                 })
