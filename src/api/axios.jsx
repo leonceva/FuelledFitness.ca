b1b413f7211a0775ded4867f402667c05f064c24
Alberto <leonceva@ualberta.ca>
2023-12-31 11:48:48 -0500

    axios endpoint for aws amplify test;

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 /* const BASE_URL = 'http://localhost:8080'; */
-const BASE_URL = 'http://192.168.2.42:8080';
+// const BASE_URL = 'http://192.168.2.42:8080';
+const BASE_URL = 'https://api.fuelledfitness.ca:8443';
 
 export default axios.create({
 	baseURL: BASE_URL,
