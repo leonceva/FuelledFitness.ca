f9735f631ed8467da743ee16a32fe374afe72b7c
leonceva <alberto.leon.c@gmail.com>
2024-06-18 21:17:31 -0400

    admin dashboard

--- src/components/AdminDashboard.jsx
+++ src/components/AdminDashboard.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import useLogout from '../hooks/useLogout';
 import useAuth from '../hooks/useAuth';
 import { jwtDecode } from 'jwt-decode';
+import styled from 'styled-components';
+
+const MOBILE_MODE_LIMIT = process.env.REACT_APP_MOBILE_MODE_LIMIT;
 
 const AdminDashboard = () => {
 	const logout = useLogout();
@@ -28,3 +31,10 @@ const AdminDashboard = () => {
 };
 
 export default AdminDashboard;
+
+export const DesktopDiv = styled.div`
+	// Hide for mobile size
+	@media screen and ((max-width: ${MOBILE_MODE_LIMIT}) or (width: ${MOBILE_MODE_LIMIT})) {
+		display: none;
+	}
+`;
