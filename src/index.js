d477ae2ed482f17fa855da5d9d2a27989171263e
Alberto <leonceva@ualberta.ca>
2024-06-28 10:47:50 -0400

    disable dev tools in production

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+// Disable React Dev Tools in production
+if (process.env.NODE_ENV === 'production') {
+	disableReactDevTools();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
